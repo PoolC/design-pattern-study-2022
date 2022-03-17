cc804bf0227ca2aa807fbb479579d411b47b0c3f
Jjungs7 <jeongy95@gmail.com>
2022-03-17 21:13:40 +0900

    bug fix

--- 03-decorator/step1-basic-example/src/main/kotlin/decorator/cafe/WhipDecorator.kt
+++ 03-decorator/step1-basic-example/src/main/kotlin/decorator/cafe/WhipDecorator.kt
@@ -3,18 +3,11 @@ package decorator.cafe
 private const val PRICE = 1500
 
 class WhipDecorator(private val beverage: Beverage) : BeverageDecorator() {
-    private var count = 3
-
     override fun getDescription(): List<String> {
         return this.beverage.getDescription() + "whip"
     }
 
     override fun cost(): Int {
-        if (count == 0) {
-            return this.beverage.cost()
-        }
-
-        count--
         return this.beverage.cost() + PRICE
     }
 }
